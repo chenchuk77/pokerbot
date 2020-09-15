75e653710e39ff280bdf7b55d87ccd786c2e5721
chenchuk77 <chenchuk@gmail.com>
2020-09-15 22:11:01 +0300

    saving state

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
 
 bot_username = 'pokerbot7777bot'
-token = '1319998611:AAHsYuusAVb-UQIw5vDodqoQFvPD3m-mils'
+# OLD token = '1319998611:AAHsYuusAVb-UQIw5vDodqoQFvPD3m-mils'
+token = '1319998611:AAFUYf1ImFb1T2q9pPNHHBYaO6ua6znzDHg'
